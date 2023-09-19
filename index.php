4e2744453d5cb67fca16ddcbabe0bc979f676a8f
Rohan indaragi <rohanindaragi03736@gmail.com>
2023-09-19 07:58:03 +0530

    charity commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php 
+
+session_start();
+require_once "./pdo.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NGO INDEX</title>
+    <?php include("bootstrap.php"); ?>
+    <link rel="stylesheet" href="bootstrap/css/style.css">
+</head>
+<body>
+<marquee width="60%" direction="right" height="59px" color="red">
+To contribute items, kindly proceed by signing in at this location.
+</marquee>
+<?php
+
+if(isset($_SESSION['volunteer_id'])){
+        require_once "volunteerIndex.php";
+}
+else if(isset($_SESSION['admin_id'])){
+    require_once "./adminIndex2.php";
+
+}
+else if(isset($_SESSION['donor_id'])){
+    require_once "donorIndex.php";  
+}
+else {
+    require_once "./navbar.php";
+    require_once "./carousel.php";
+}
+ ?>
+ 
+</body>
+</html>
